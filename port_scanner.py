c4ca6e555f932ee11994e130584803c4e5a9a666
Saja6 <110252798+Saja6@users.noreply.github.com>
2026-05-24 18:06:04 -0400

    Add completion message after port scan
    Added print statement to indicate completion of port scan.

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,3 @@
-# Copyright 2026 Saja6: https://github.com/Saja6
 import socket
 # we will scan ports over the designated range
 # @param: the range of ports such as (1, 1024) to scan
@@ -6,6 +5,7 @@ import socket
 def scanPorts(portRange):
     IPs = ["192.168.1.185"] # add as many IPs whose ports will be scanned as need be.
     for ip in IPs:
+        print(f"Scanning ports on {ip}...")
         for port in portRange:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(0.5) # wait for this long before timing out
@@ -17,7 +17,9 @@ def scanPorts(portRange):
                     print(f"Could not resolve hostname/IP: {ip}")
                     break
                 except Exception as e: pass
+        print(f"Connection to {ip} closed.")
 
 if __name__ == "__main__":
     # pass a range or custom tuple of ports to scan
     scanPorts(range(1,1024))
+    print("Port scan complete.")
