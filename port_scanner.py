a8e903f0d807e1198e47e39e5afc98e47a37e11c
Saja6 <110252798+Saja6@users.noreply.github.com>
2026-06-13 19:13:01 -0400

    Enhance output formatting in port scanner

--- port_scanner.py
+++ port_scanner.py
@@ -5,7 +5,7 @@ import socket
 def scanPorts(portRange):
     IPs = ["192.168.1.185"] # add as many IPs whose ports will be scanned as need be.
     for ip in IPs:
-        print(f"Scanning ports on {ip}...")
+        print(f"::: Scanning ports on {ip}...")
         for port in portRange:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.settimeout(0.5) # wait for this long before timing out
@@ -17,9 +17,9 @@ def scanPorts(portRange):
                     print(f"Could not resolve hostname/IP: {ip}")
                     break
                 except Exception as e: pass
-        print(f"Connection to {ip} closed.")
+        print(f"::: Connection to {ip} closed.")
 
 if __name__ == "__main__":
     # pass a range or custom tuple of ports to scan
     scanPorts(range(1,1024))
-    print("Port scan complete.")
+    print("::: Port scan complete.")
