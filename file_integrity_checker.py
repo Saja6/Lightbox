683be0cf5301f986edd0d3d3275188702b15fcee
Saja6 <110252798+Saja6@users.noreply.github.com>
2026-09-14 14:31:58 -0400

    Enhance file integrity checker with user prompts

--- file_integrity_checker.py
+++ file_integrity_checker.py
@@ -1,9 +1,10 @@
 # Copyright 2026 Saja6: https://github.com/Saja6
 import hashlib
 import os
+import sys
 import time
 import datetime
-targets = [] # the directories we will compute file hashes inside are here (add yours).
+targets = ["/Users/sj/Downloads/"] # the directories we will compute file hashes inside are here (add yours).
 # we will walk each directory in the list of targets recursively and compute a hash for each file inside it
 #   @param: none
 #   @return: the modified hashMap variable containing the
@@ -11,7 +12,7 @@ targets = [] # the directories we will compute file hashes inside are here (add 
 #       directory located in targets. if targets is empty,
 #       computeHashes() will return an empty hashMap.
 def computeHashes():
-    print("::: Computing hashes...")
+    print("🕒 ::: Computing hashes...")
     hashMap = {}  # here, a file path will be mapped to its corresponding computed hash
     if targets == []: return {}
     for thisTarget in targets: # for each directory, we will walk recursively through it
@@ -20,6 +21,7 @@ def computeHashes():
                 try:
                     hasher = hashlib.sha256() # compute an SHA256 hash
                     filePath = os.path.join(root, fileName) # make sure we got the full path!
+                    print(f"🕒 ::: Computing hash for {fileName}...")
                     with open(filePath, 'rb') as f:
                         chunk = f.read(8192)
                         while chunk: # as long as we can read from the file, update the hash as input text grows
@@ -27,16 +29,17 @@ def computeHashes():
                             chunk = f.read(8192)
                     fileHash = hasher.hexdigest() # allow the hash to be in hexadecimal for safety
                     hashMap[filePath] = fileHash # next, make a new entry in the hashMap
+                    print(f"✅ ::: Hash calculated for {fileName}...")
                 except (FileNotFoundError, PermissionError):
                     continue
-    print("::: Hash computation complete.")
+    print("✅ ::: Hash computation complete.")
     return hashMap
 
 # we will write our baseline hashMap results to a file called hashes.log.
 #   @param: hashMap
 #   @return: none.
 def writeMap(map):
-    print("::: Writing hashes to hashes.log...")
+    print("🕒 ::: Writing hashes to hashes.log...")
     with open('hashes.log', 'w') as f:
         for fileName, hash in map.items():
             f.write(fileName + ' | ' + hash + '\n')
@@ -47,7 +50,7 @@ def writeMap(map):
 #   @return: a tuple containing the type of action, the path to file, and the old hash and path stored on
 #       file in the new map (if needed).
 def compareHashes(oldMap, newMap):
-    print("::: Comparing hashes to stored hashes...")
+    print("🕒 ::: Comparing hashes to stored hashes...")
     changes = []
     for path, oldHash in oldMap.items(): # go through the hash and file path in the old map
         if path not in newMap: # we can't find the path; it was deleted
@@ -60,21 +63,32 @@ def compareHashes(oldMap, newMap):
     return changes
 
 if __name__ == '__main__':
-    interval = 300 # every 5 minutes we will run a file integrity check
-    baselineMap = computeHashes()
-    with open("results.rpt", "a") as f:
-        while True:
-            time.sleep(interval) # wait then compute the hashes
+    print("::: Welcome to FICT, the File Integrity Checking Tool!\n----------------------------------------------------------------\n❗ "
+          "Note: this tool is designed for use on folders containing confidential,\n"
+          "personal, or sensitive information that is only accessible by authorized personnel.\n"
+          "Using this tool on folders containing frequently-modified or edited content may cause\n"
+          "this tool to produce many false positives. If using this tool on a suitable set of information,\n"
+          "please enter 'C' to continue with the file integrity check for your required directories.\n"
+          "Otherwise, please press any other key to exit the program.\n")
+    entry = input("Continue? [C/c]: ")
+    if entry == "C" or entry == "c":
+        baselineMap = computeHashes()
+        with open("results.rpt", "a") as f:
             newMap = computeHashes()
             results = compareHashes(baselineMap, newMap) # compare the computed hashes to what we have on file
             timestamp = datetime.datetime.now().strftime("%B %d %Y at %I:%M:%S %p")
             if results:
                 for result in results: # iterate through the results and get its contents:
                     action, filepath, oldhash, newhash = result[3], result[0], result[1], result[2]
-                    line = f"[ALERT] File changes detected | {action} | {filepath} | {oldhash} | {newhash} on {timestamp}\n"
+                    print("------------------------------------------------")
+                    line = f"🚨::: File changes detected | {action} | {filepath} | {oldhash} | {newhash} on {timestamp}"
+                    print(line)
                     f.write(line)
             else:
-                line = f"[INFO] File integrity check complete | {timestamp}\n"
+                print("------------------------------------------------")
+                line = f"✅ File integrity check complete! No outstanding changes detected at {timestamp}."
+                print(line)
                 f.write(line)
             f.flush()
             baselineMap = newMap # update our map based off our findings
+    else: sys.exit(1)
