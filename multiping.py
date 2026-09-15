bd48a72083b76358198f09431d5206f13ae0510f
Saja6 <110252798+Saja6@users.noreply.github.com>
2026-09-14 20:00:57 -0400

    Enhance ping output with emojis and summary report
    Updated ping function to include emojis in output and modified result handling.

--- multiping.py
+++ multiping.py
@@ -9,7 +9,7 @@ import time
 #           if the ping throws an exception, it will include the error
 #           in the dictionary as well.
 def ping(host):
-    print(f"::: Pinging {host}...")
+    print(f"🕒 ::: Pinging {host}...")
     start = time.perf_counter()
     try:
         # NOTE: on Windows, replace "-c" with "-n"
@@ -19,6 +19,7 @@ def ping(host):
         return {
             "host": host,
             "status": "UP",
+            "error": "No errors detected",
             "time": round(elapsed, 2)
         }
     except Exception as e:
@@ -29,19 +30,24 @@ def ping(host):
             "error": str(e),
             "time": round(elapsed, 2)
         }
-    print("::: Ping complete.")
 
 if __name__ == "__main__":
-    hostList = ["wholefoods.com", "amazon.com", "google.com", "192.168.1.166", "172.16.17.32", "192.168.1.156", "192.168.1.1", "192.168.1.100"] # change these to your list of hosts!
-    futures = []
+    hostList = ["wholefoods.com", "google.com"] # change these to your list of hosts!
+    results = []
+    errorCount = 0
     # use a thread pool executor for improved performance
     with ThreadPoolExecutor(max_workers = min(3, len(hostList))) as executor:
         for host in hostList:
-            future = executor.submit(ping, host) # submit our function to the pool
-            futures.append(future)
-    with open(f"ping_report_{time.strftime("%Y%b%d-%I:%M%p")}.txt", "w") as f:
+            result = executor.submit(ping, host) # submit our function to the pool
+            results.append(result)
+    with open(f"ping_report.txt", "w") as f:
         f.write("**** BEGIN PING SESSION SUMMARY ****\n")
-        for future in as_completed(futures): # print out the results of each ping.
+        for future in as_completed(results): # print out the results of each ping.
             futureMap = future.result()
+            print(f"- Ping result for: {futureMap['host']}:")
+            print(f"\t🕒Elapsed time: {futureMap['time']}\n\t❓Status: {futureMap['status']}\n\t⚠️ Errors: {futureMap['error']}\n")
+            if(futureMap['error'] != "No errors detected"): errorCount += 1
             f.write(f"{futureMap['host']}: {futureMap['status']} | ELAPSED TIME: ({futureMap['time']}s)\n")
         f.write("**** END PING SESSION SUMMARY ****")
+    if errorCount > 0: print("⚠️ ::: Warning: some hosts failed to be pinged. Please review results.")
+    print("✅ ::: Done! Generated copy of results in \"ping_report.txt\".")
