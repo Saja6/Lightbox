f6ec9132367224af64afa4c7c588355b3347d2e7
Saja6 <110252798+Saja6@users.noreply.github.com>
2026-09-18 19:38:47 -0400

    Add fastfetch.py for system information retrieval

---
+++ UltimateRaspberryPiDesktop/fastfetch.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+from pynput import keyboard
+import sys
+
+def exitthis(key):
+    if key == keyboard.Key.Esc: sys.exit(0)
+    
+if __name__ == "__main__":
+    print("::: Getting system information...")
+    try:
+        subprocess.run( ["sudo", "fastfetch"],check = True)
+    except subprocess.CalledProcessError as e:
+        print(f"::: Command failed with exit code {e.returncode}")
+    print("::: Press ESC to exit.")
+    listener = keyboard.Listener(on_press = exitthis)
+    listener.start()
+    listener.join()
